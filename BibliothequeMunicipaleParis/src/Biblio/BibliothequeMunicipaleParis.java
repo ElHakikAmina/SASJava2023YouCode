6429deab65a71da29b52581792655a8e1cb0e7bd
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-09-01 10:55:19 +0000

    creation d'une menu qui contient les use cases

---
+++ BibliothequeMunicipaleParis/src/Biblio/BibliothequeMunicipaleParis.java
@@ -0,0 +1,34 @@
+package Biblio;
+
+import java.util.Scanner;
+
+public class BibliothequeMunicipaleParis {
+	public static void afficherMenu() {
+        System.out.println("\nMenu :");
+        System.out.println("1. Ajouter un livre");
+        System.out.println("2. Afficher la liste des livres disponibles");
+        System.out.println("3. Rechercher un livre");
+        System.out.println("4. Emprunter un livre");
+        System.out.println("5. Retourner un livre");
+        System.out.println("6. Afficher la liste des livres empruntés");
+        System.out.println("7. Supprimer un livre");
+        System.out.println("8. Modifier les informations d'un livre");
+        System.out.println("9. Statistique");
+        System.out.println("0. Quitter");
+        System.out.print("Choisissez une option : ");
+    }
+    public static void main(String[] args) {
+        
+    	afficherMenu();
+        
+    	
+    	
+    	
+    	 
+        
+    	
+    	
+    	
+    	
+    }
+}
