99d304fe707f2cb4f8f5d32172981200f6976c2a
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-09-08 21:49:57 +0000

    creation d'une classe pour la cnx à la base de donnée avec Singleton

---
+++ BibliothequeJavaSASYouCode/src/biblio/Biblio.java
@@ -0,0 +1,15 @@
+package biblio;
+
+import java.sql.Connection;
+public class Biblio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ConnexionDB connexionDB = ConnexionDB.getInstance();
+	        Connection connexion = connexionDB.getConnexion();
+System.out.println("hello");
+	//ajouter un live
+
+	}
+
+}
