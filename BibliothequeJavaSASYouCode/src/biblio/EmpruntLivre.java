968ad8670c731599627d7305e96d326c9a0ebe4f
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-09-10 02:45:20 +0000

    emprunt livre fonction

--- BibliothequeJavaSASYouCode/src/biblio/EmpruntLivre.java
+++ BibliothequeJavaSASYouCode/src/biblio/EmpruntLivre.java
@@ -1,7 +1,11 @@
 package biblio;
 
 import java.util.Date;
+import java.time.LocalDate;
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class EmpruntLivre {
 	 private int id;
@@ -9,6 +13,9 @@ public class EmpruntLivre {
 	 private int id_emprunteur;
 	 private Date date_emprunt;
 	 private Date date_retour;
+	 
+	 public static Connection connexion = ConnexionDB.getInstance().getConnexion();
+
 	 
 	 private ISBN isbnClasse = new ISBN();
 	 private Emprunteur emprunteur = new Emprunteur();
@@ -30,6 +37,45 @@ public class EmpruntLivre {
 			 if(isbnClasse.ISBNDispo(this.livre_ISBN))
 			 {
 				 emprunteur.ajoutEmprunteur();
+				 
+				 
+				 
+				 
+				 // Générer automatiquement la date d'emprunt (date actuelle)
+	                LocalDate dateEmprunt = LocalDate.now();
+	                System.out.println("Date d'emprunt : " + dateEmprunt);
+	                
+	                
+	             // Demander la date de retour à l'utilisateur
+	                System.out.println("Entrez la date de retour (AAAA-MM-JJ) :");
+	                String dateRetourStr = scanner.nextLine();
+	                LocalDate dateRetour = LocalDate.parse(dateRetourStr);
+	                
+	                
+	                
+	             // Insérer les informations dans la table emprunt_livre
+	                try {
+	                    String requeteInsertion = "INSERT INTO emprunt_livre (livre_ISBN, id_emprunteur, Date_emprunt, Date_retour) VALUES (?, ?, ?, ?)";
+	                    PreparedStatement preparedStatement = connexion.prepareStatement(requeteInsertion);
+	                    preparedStatement.setString(1, livre_ISBN);
+	                    preparedStatement.setInt(2, emprunteur.getId()); // Supposons que vous avez un getter pour l'ID de l'emprunteur
+	                    preparedStatement.setDate(3, java.sql.Date.valueOf(dateEmprunt)); // Convertir LocalDate en java.sql.Date
+	                    preparedStatement.setDate(4, java.sql.Date.valueOf(dateRetour)); // Convertir LocalDate en java.sql.Date
+
+	                    int lignesAffectees = preparedStatement.executeUpdate();
+
+	                    if (lignesAffectees > 0) {
+	                        System.out.println("L'emprunt a été enregistré avec succès.");
+	                    } else {
+	                        System.out.println("L'enregistrement de l'emprunt a échoué.");
+	                    }
+
+	                    preparedStatement.close();
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	                }
+	                /////////////////////////
+				 
 			 }else {
 				 System.out.println("ISBN n'est pas disponible");
 			 }
--- BibliothequeJavaSASYouCode/src/biblio/Emprunteur.java
+++ BibliothequeJavaSASYouCode/src/biblio/Emprunteur.java
@@ -1,12 +1,15 @@
 package biblio;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+ 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Emprunteur {
-	//private int id;
+	private int id;
 	private String nom_complet; 
 	private int numTel;  
 	private String adresse;
@@ -14,6 +17,11 @@ public class Emprunteur {
 	public static Connection connexion = ConnexionDB.getInstance().getConnexion();
 	
 	private static Scanner scanner = new Scanner(System.in);
+	
+	public int getId()
+	{
+		return this.id;
+	}
 	
 	public void ajoutEmprunteur() {
 	    System.out.println("Donner le nom complet : ");
@@ -26,22 +34,29 @@ public class Emprunteur {
 	    PreparedStatement preparedStatement = null;
 
 	    try {
-	        
-
-	        
+	         
 	        String query = "INSERT INTO emprunteur (nom_complet, numTel, adresse) VALUES (?, ?, ?)";
 
 	        
-	        preparedStatement = connexion.prepareStatement(query);
+	        preparedStatement = connexion.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
 	        preparedStatement.setString(1, this.nom_complet);
 	        preparedStatement.setInt(2, this.numTel);
 	        preparedStatement.setString(3, this.adresse);
 
 	        
 	        int rowsInserted = preparedStatement.executeUpdate();
+	        ResultSet rs = preparedStatement.getGeneratedKeys();
+	        if (rs.next()) {
+	        	  int lastInsertedID = rs.getInt(1);
+	                //System.out.println("Last Inserted ID: " + lastInsertedID);
+	                this.id=lastInsertedID;
+            }
 
 	        if (rowsInserted > 0) {
+	           
+	            
 	            System.out.println("Emprunteur ajouté avec succès.");
+	            
 	        } else {
 	            System.out.println("L'ajout de l'emprunteur a échoué.");
 	        }
@@ -55,12 +70,7 @@ public class Emprunteur {
 	            } catch (SQLException e) {
 	                e.printStackTrace();
 	            }
-	        }
-	        
-	        /*if (connexion != null) {
-	            ConnexionBDD.fermerConnexion(connexion);
-	        }*/
+	        }       
 	    }
 	}
-
 }
